013aeec532e9c75449d1ca991eefbc66ceb10e67
danish_ds <danishdeepasoman02@gmail.com>
2024-06-03 20:50:33 +0530

    Use of functions

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('hello');
+//to display the variable element
+var person = {
+    firstName: "Bucky",
+    LastName: "Nobert",
+    age: 25
+};
+
+//to show a simple output
+    console.log('hello');  
+
+//to add two number 
+function addnumber(a,b){
+    return a+b;
+}
+console.log(addnumber(7,2));
+
+// to use a variable to pass the output 
+var PrintBacon = function(){
+    console.log{"bacon is the best man "};
+};
+PrintBacon();
