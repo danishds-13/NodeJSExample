3d690e4fe11709c5522a9f42d8c23f7aa6b79221
danish_ds <danishdeepasoman02@gmail.com>
2024-06-04 23:23:04 +0530

    Handling multiple requests
    Handling multiple requests

--- app.js
+++ app.js
@@ -16,6 +16,26 @@ console.log(addnumber(7,2));
 
 // to use a variable to pass the output 
 var PrintBacon = function(){
-    console.log{"bacon is the best man "};
+    console.log("bacon is the best man ");
 };
 PrintBacon();
+
+// for food example
+function placeAnOrder(orderNumber){
+    console.log("customer order:", orderNumber);
+
+    CookedDeliverFood{function() {
+        console.log("delivered food order:", orderNumber);
+    }};
+}
+
+//to simulate a 5 second operation 
+function cookAndDeliverFood(callback){
+    setTimeout(callback, 5000);
+}
+
+//Stimulate users web request 
+placeAnOrder(1);
+placeAnOrder(2);
+placeAnOrder(3);
+placeAnOrder(4);
